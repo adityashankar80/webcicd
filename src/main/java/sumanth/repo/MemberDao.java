be1150ee0a7d85de16542090a76a66d19296f12a
Ubuntu <ubuntu@ip-172-31-36-37.us-east-2.compute.internal>
2019-10-21 08:37:16 +0000

    this is my first command

---
+++ src/main/java/sumanth/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package sumanth.repo;
+
+import java.util.List;
+
+import sumanth.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
